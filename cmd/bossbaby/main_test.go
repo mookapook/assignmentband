02e0aaef3b6f9dd15dd7b10ccaee650792858218
Prakit Worrarakuksorn <prakit@bunditcenter.com>
2024-07-03 16:57:09 +0700

    Update main_test.go

--- cmd/bossbaby/main_test.go
+++ cmd/bossbaby/main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestBossBabyRevenge_boss_expected_good_boy(t *testing.T) {
+func TestBossBabyCheck_expected_manygood_boy(t *testing.T) {
 	shot := "sSRSSRRRX"
 	want := "Good boy"
 	if got := checkBehavior(shot); got != want {
@@ -12,23 +12,24 @@ func TestBossBabyRevenge_boss_expected_good_boy(t *testing.T) {
 	}
 }
 
-func TestBossBabyRevenge_boss_expected_many(t *testing.T) {
+func TestBossBabyCheck_expected_many(t *testing.T) {
 	tests := map[string]string{
-		"SRSSRRR":    "Good boy", // กรณีทั่วไปที่ Boss Baby ได้แก้แค้นครบทุกการยิง
-		"RSSRR":      "Bad boy",  // กรณีที่ Boss Baby ยิงกลับก่อนจะถูกยิง
-		"SSSRRRRS":   "Bad boy",  // กรณีที่การยิงสุดท้ายไม่ได้ถูกแก้แค้น
-		"SRRSSR":     "Bad boy",  // กรณีที่มีการยิงที่ไม่ถูกแก้แค้น
-		"SSRSRRR":    "Good boy", // กรณีที่ Boss Baby ได้แก้แค้นครบทุกการยิง
-		"RRRR":       "Bad boy",  // กรณีที่มีแต่การยิงกลับโดยไม่มีการยิงใส่ก่อน
-		"SSSS":       "Bad boy",  // กรณีที่มีแต่การยิงใส่โดยไม่มีการยิงกลับ
-		"SRRS":       "Bad boy",  // กรณีที่มีการยิงแต่ไม่ได้แก้แค้นครบ
-		"SRRSRR":     "Good boy", // กรณีที่ Boss Baby ได้แก้แค้นครบทุกการยิง
-		"SR":         "Good boy", // กรณีที่มีการยิงและการแก้แค้นเพียงครั้งเดียว
-		"R":          "Bad boy",  // กรณีที่มีการยิงกลับโดยไม่มีการยิงใส่ก่อน
-		"S":          "Bad boy",  // กรณีที่มีการยิงใส่โดยไม่มีการยิงกลับ
-		"SRRSRRRSS":  "Bad boy",  // กรณีที่มีการยิงสุดท้ายไม่ได้ถูกแก้แค้น
-		"SSSSRRRRRR": "Good boy", // กรณีที่มีการยิงใส่หลายครั้งและการแก้แค้นครบทุกการยิง
-		"":           "",
+		"SRSSRRR":    "Good boy", // General case where Boss Baby has revenged every shot
+		"RSSRR":      "Bad boy",  // Case where Boss Baby shoots back before being shot at
+		"SSSRRRRS":   "Bad boy",  // Case where the last shot is not revenged
+		"SRRSSR":     "Bad boy",  // Case where some shots are not revenged
+		"SSRSRRR":    "Good boy", // Case where Boss Baby has revenged every shot
+		"RRRR":       "Bad boy",  // Case with only revenge shots without any prior shots
+		"SSSS":       "Bad boy",  // Case with only shots without any revenge
+		"SRRS":       "Bad boy",  // Case where not all shots are revenged
+		"SRRSRR":     "Good boy", // Case where Boss Baby has revenged every shot
+		"SR":         "Good boy", // Case with only one shot and one revenge
+		"R":          "Bad boy",  // Case with a revenge shot without any prior shots
+		"S":          "Bad boy",  // Case with a shot without any revenge
+		"SRRSRRRSS":  "Bad boy",  // Case where the last shot is not revenged
+		"SSSSRRRRRR": "Good boy", // Case with multiple shots and sufficient revenge
+		"XEEESS":     "Bad boy",
+		"XEEE":       "Good boy",
 	}
 	for s, index := range tests {
 		if got := checkBehavior(s); got != index {
